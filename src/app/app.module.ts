0927bd10c2d7c0d1dfb67891900791797a60de45
thulisile98 <thulisile.ma98@gmail.com>
2022-12-12 23:27:21 +0200

    generated about component, portfolio component, certificates and contacts, configured paths in the app routing module  and populated the about component with details and lastly added routerlinks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CertificatesComponent } from './certificates/certificates.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalDetailsComponent
+    PersonalDetailsComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactsComponent,
+    CertificatesComponent
   ],
   imports: [
     BrowserModule,
