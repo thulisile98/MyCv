3fb0f22279a7a7bea23d61ed80d9b2bbb792d1ad
thulisile98 <thulisile.ma98@gmail.com>
2023-01-05 01:12:05 +0200

    CICD

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDu3vsNrMk-2p_gI9mykYWRtuRfVH4rf1g",
+        authDomain: "web-cv-25a25.firebaseapp.com",
+        projectId: "web-cv-25a25",
+        storageBucket: "web-cv-25a25.appspot.com",
+        messagingSenderId: "136752693468",
+        appId: "1:136752693468:web:8520c1480920db81587419",
+        measurementId: "G-7L4ET1BVQ0"
+    },
+
+};
